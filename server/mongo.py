f49440d48d9d38961be321941006921a3862a67a
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-05-02 19:06:28 -0400

    update reqs + use os module + add Procfile

--- server/mongo.py
+++ server/mongo.py
@@ -1,11 +1,10 @@
 import pymongo
-from dotenv import dotenv_values
+import os
 
 def db_cursor():
-    config = dotenv_values(".env")
-    MONGO_DB_USERNAME = config["MONGO_DB_USERNAME"]
-    MONGO_DB_PASSWORD = config["MONGO_DB_PASSWORD"]
-    MONGO_DB_ADDRESS = config["MONGO_DB_ADDRESS"]
+    MONGO_DB_USERNAME = os.environ["MONGO_DB_USERNAME"]
+    MONGO_DB_PASSWORD = os.environ["MONGO_DB_PASSWORD"]
+    MONGO_DB_ADDRESS = os.environ["MONGO_DB_ADDRESS"]
 
     conn_str = f"mongodb+srv://{MONGO_DB_USERNAME}" + \
         f":{MONGO_DB_PASSWORD}@{MONGO_DB_ADDRESS}" + \
